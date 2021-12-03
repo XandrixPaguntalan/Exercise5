50bb51695bf74ac2d1f9dbd6f37e347d6e1d9dad
XandrixPaguntalan <xandrixjill@Gmail.com>
2021-12-03 10:13:13 +0800

    Initial commit

---
+++ exerciseFive.py
@@ -0,0 +1,7 @@
+firstSide = int(input("Enter 1st side:"))
+secondSide = int(input("Enter 2nd side:"))
+thirdSide = int(input("Enter 3rd side:"))
+if firstSide == secondSide and thirdSide:
+    print("The triangle is equilateral.")
+else:
+    print("The triangle is not equilateral.")
